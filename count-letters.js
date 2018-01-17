41a8402883ecba69298163d38c9d867f1dde7bc1
Joel Tannas <jtannas@gmail.com>
2018-01-17 13:39:26 -0700

    refactored from O(len*letters) to O(len)

--- count-letters.js
+++ count-letters.js
@@ -2,10 +2,10 @@
 
 const countLetters = function returnCountOfEachCharInStringExcludingSpaces(str) {
   const strNoSpaces = str.replace(/ /g, '');
-  const charSet = new Set(strNoSpaces);
   const charCounts = {};
-  for (const char of charSet) {
-    charCounts[char] = strNoSpaces.match(new RegExp(char, 'g')).length;
+  for (let char of strNoSpaces) {
+    if (charCounts[char] === undefined) { charCounts[char] = 0; }
+    charCounts[char] += 1;
   }
   return charCounts;
 };
